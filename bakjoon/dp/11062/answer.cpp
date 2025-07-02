bb9742dd780f0d12523b6ca00867b8cc38b913db
JEONG-MUK-LIM <126922728+JEONG-MUK-LIM@users.noreply.github.com>
2025-07-03 00:27:05 +0900

    Update answer.cpp

--- bakjoon/dp/11062/answer.cpp
+++ bakjoon/dp/11062/answer.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
 using namespace std;
 
 int main(){
@@ -19,16 +18,17 @@ int main(){
             sum[i] = sum[i-1] + arr[i];
         }
         vector<vector<int>> dp1(n+1,vector<int>(n+1,0));
-        vector<vector<int>> dp2(n+1,vector<int>(n+1,0));
+        for(int i=1; i<=n; i++) {
+            dp1[i][i] = arr[i]; // 본인차례일 때 i부터 j까지 카드가 있을 때 만들 수 있는 최댓값
+        }
 
         for(int len = 1; len <= n; len++){
             for(int i=1; i + len <= n; i++){
                 int j = i + len;
-                if(len % 2 == 0){
-                    dp1[i][j] = max(arr[i] + , arr[j] + )
-                    dp2[i][j] = sum[j] - sum[i-1] - dp1[i][j];
-                }
+                dp1[i][j] = max(arr[i] + sum[j] - sum[i] - dp1[i+1][j]
+                    , arr[j] + sum[j-1] - sum[i-1] - dp1[i][j-1]);
             }
         }
+        cout << dp1[1][n] << '\n';
     }
 }       
